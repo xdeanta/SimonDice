2437709024cca23fd974473492564570d0e25f90
Xavier <xdeantabelisario@danielcastelao.org>
2020-10-28 20:08:38 +0100

    Funciones basicas añadidas

--- app/src/main/java/com/dam2/xavier/simondice/MainActivity.kt
+++ app/src/main/java/com/dam2/xavier/simondice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.dam2.xavier.simondice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addToSec(sec : MutableList<Int>)  {
+        val numb= Random.nextInt(4) + 1
+        sec.add(numb)
+    }
+
+    fun checkSec(sec : MutableList<Int>, secUsr : MutableList<Int>) : Boolean {
+        return sec == secUsr
     }
 }
