339bcd3b7b709b5eafcf4b5ed09d33a332756f34
Xavier <xdeantabelisario@danielcastelao.org>
2020-11-29 04:21:14 +0100

    Proyecto completamente refactorizado al modelo MVVM y limpieza de codigo no utilizado

--- app/src/main/java/com/dam2/xavier/simondice/MainActivity.kt
+++ app/src/main/java/com/dam2/xavier/simondice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dam2.xavier.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
 import android.widget.Button
 import android.widget.Toast
 
@@ -15,29 +16,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var sec: MutableList<Int>
+        var state : Boolean
         val button_red : Button = findViewById(R.id.rojo)
         val button_yellow : Button = findViewById(R.id.amarillo)
         val button_blue : Button = findViewById(R.id.azul)
         val button_green : Button = findViewById(R.id.verde)
         val button_start : Button = findViewById(R.id.start)
         val button_check : Button = findViewById(R.id.check)
+        val Btnscolor = listOf(button_red,button_green,button_yellow,button_blue)
         val toast = Toast.makeText(applicationContext,"Juego terminado", Toast.LENGTH_SHORT)
         val toast2 = Toast.makeText(applicationContext,"Inicio", Toast.LENGTH_SHORT)
 
         val ModeloJuego by viewModels<liveData>()
 
+        ModeloJuego.gameState.observe(this, Observer{
+            gs -> state = gs
+            if(state) {
+                button_red.setEnabled(false)
+                button_yellow.setEnabled(false)
+                button_blue.setEnabled(false)
+                button_green.setEnabled(false)
+                button_check.setEnabled(false)
+            }else{
+                button_red.setEnabled(true)
+                button_yellow.setEnabled(true)
+                button_blue.setEnabled(true)
+                button_green.setEnabled(true)
+                button_check.setEnabled(true)
+            }
+        })
+
+        ModeloJuego.secuenciaJ.observe(this, Observer{
+            ModeloJuego.showSec(Btnscolor)
+        })
+
         button_start.setOnClickListener{
             toast2.show()
-            sec=ModeloJuego.init_game()
-            showSec(sec)
+            ModeloJuego.init_game()
         }
 
         button_check.setOnClickListener{
-            if(ModeloJuego.checkSec()){
-                sec=ModeloJuego.getSec()
-                showSec(sec)
-            }else{
+            if(!ModeloJuego.checkSec()){
                 toast.show()
             }
         }
@@ -52,43 +71,6 @@ class MainActivity : AppCompatActivity() {
         }
         button_blue.setOnClickListener{
             ModeloJuego.addUserSec(4)
-        }
-        //showSec(sec)
-
-    }
-
-    fun showSec(sec: MutableList<Int>){
-        val button_red : Button = findViewById(R.id.rojo)
-        val button_yellow : Button = findViewById(R.id.amarillo)
-        val button_blue : Button = findViewById(R.id.azul)
-        val button_green : Button = findViewById(R.id.verde)
-
-        CoroutineScope(Dispatchers.Main).launch{
-            for (color in sec){
-                when(color){
-                1 -> {
-                    button_red.setPressed(true)
-                    delay(1000)
-                    button_red.setPressed(false)
-                }
-                2 -> {
-                    button_green.setPressed(true)
-                    delay(1000)
-                    button_green.setPressed(false)
-                }
-                3 -> {
-                    button_yellow.setPressed(true)
-                    delay(1000)
-                    button_yellow.setPressed(false)
-                }
-                4 -> {
-                    button_blue.setPressed(true)
-                    delay(1000)
-                    button_blue.setPressed(false)
-                }
-                }
-            }
-
         }
     }
 }
--- app/src/main/java/com/dam2/xavier/simondice/liveData.kt
+++ app/src/main/java/com/dam2/xavier/simondice/liveData.kt
@@ -1,52 +1,58 @@
 package com.dam2.xavier.simondice
 
+import android.util.Log
+import android.widget.Button
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
-class liveData : ViewModel(){
+class liveData : ViewModel() {
     val secuenciaJ = MutableLiveData<MutableList<Int>>()
     val secuenciaU = MutableLiveData<MutableList<Int>>()
     val gameState = MutableLiveData<Boolean>()
 
-    init{
+    init {
         secuenciaU.value = mutableListOf<Int>()
-        secuenciaJ.value= mutableListOf<Int>()
-        gameState.value= false
+        secuenciaJ.value = mutableListOf<Int>()
+        gameState.value = true
     }
 
-    fun init_game() : MutableList<Int>{
-        gameState.value=false;
+    fun init_game() {
+        gameState.value = false;
         reset()
         addToSecu()
-        return secuenciaJ.value!!
     }
 
-    private fun addToSecu()  {
-        val numb= Random.nextInt(4) + 1
+    private fun addToSecu() {
+        val numb = Random.nextInt(4) + 1
         //val numb = 1;
         secuenciaJ.value?.add(numb)
+        secuenciaJ.postValue(secuenciaJ.value)
     }
 
-    fun checkSec() : Boolean {
-        var ret=false
-        if(secuenciaJ.value == secuenciaU.value && gameState.value == false){
+    fun checkSec(): Boolean {
+        var ret = false
+        if (secuenciaJ.value == secuenciaU.value && gameState.value == false) {
             addToSecu()
             secuenciaU.value?.clear()
-            ret=true;
-        }else{
-            gameState.value=true;
+            ret = true;
+        } else {
+            gameState.value = true;
         }
         return ret;
     }
 
-    private fun reset(){
+    private fun reset() {
         secuenciaJ.value?.clear()
         secuenciaU.value?.clear()
     }
 
-    fun addUserSec(color: Int){
-        when(color){
+    fun addUserSec(color: Int) {
+        when (color) {
             1 -> secuenciaU.value?.add(1)
             2 -> secuenciaU.value?.add(2)
             3 -> secuenciaU.value?.add(3)
@@ -54,7 +60,17 @@ class liveData : ViewModel(){
         }
     }
 
-    fun getSec(): MutableList<Int>{
+    fun getSec(): MutableList<Int> {
         return secuenciaJ.value!!
+    }
+
+    fun showSec(listButton: List<Button>) {
+        CoroutineScope(Dispatchers.Main).launch {
+            for (colors in secuenciaJ.value!!) {
+                listButton.get(colors-1).setPressed(true)
+                delay(1000)
+                listButton.get(colors-1).setPressed(false)
+            }
+        }
     }
 }
